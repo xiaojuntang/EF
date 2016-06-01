419ec3b64dac30bcb80e4b03ea9ecdc92e4f109a
tangxiaojun <417281862@qq.com>
2016-06-01 18:20:36 +0800

    修改

--- EF.Web/FE.Dao/Implements/BaseRepository.cs
+++ EF.Web/FE.Dao/Implements/BaseRepository.cs
@@ -3,6 +3,8 @@ using EntityFramework.BulkInsert.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
